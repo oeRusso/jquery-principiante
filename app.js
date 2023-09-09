089f069831c55a65c595748a4c55123b75b80be3
Esteban Russo <oerusso28@gmail.com>
2023-09-09 17:16:24 -0300

    selectores

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-        alert('Hola mundo desde jquery');
+        // alert('Hola mundo desde jquery');
+        // let boton = $("button").html();
+        // console.log(boton);
+        // $('button').html('aver aver');
+        $('.btn').html('cambiado desde la clase');
+        $('#boton').html('cambiado desde el id');
+
+
 })
